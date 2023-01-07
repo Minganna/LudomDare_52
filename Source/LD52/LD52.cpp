f45ca6f89923e213bb747a471ec063f3f269cf19
unknown <s4929892@bournemouth.ac.uk>
2023-01-07 08:54:08 +0000

    Initial commit, added Multiplayer logic

---
+++ Source/LD52/LD52.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LD52.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LD52, "LD52" );
